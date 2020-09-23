66a6cc695cf97f3da2c27698abe5a9e516766740
Aditya <aditya@idealittechno.com>
2020-09-23 19:27:56 +0530

    Added product list functionality

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now)
+
+
+class Product(models.Model):
+    """ This the default Product class """
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price_ht = models.FloatField(blank=True)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from django.views.generic import ListView
+
+from cart.models import Product
+
+
+class ProductList(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, "product_list.html", {"products": products})
+
+
+def delete_product(product_id):
+    product = Product.objects.get(id=product_id)
+    product.delete()
